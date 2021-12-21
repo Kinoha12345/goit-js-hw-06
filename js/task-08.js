eb80d563968f00b2d064a8fe87d966a14f8320bd
Kinoha12345 <filin.vlad.v@gmail.com>
2021-12-21 10:30:50 +0200

    sasaaa

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,25 @@
 const form = document.querySelector('.login-form');
-const input = document.querySelectorAll('input');
-const inputEm = input[0];
-const inputPas = input[1];
-const button = document.querySelector('button');
+// const input = document.querySelectorAll('input');
+// const inputEm = input[0];
+// const inputPas = input[1];
+// const button = document.querySelector('button');
 
 form.addEventListener('submit', (event) => {
 event.preventDefault();
-const {
-    elements: {inputEm, inputPas},
-} = event.currentTarget;
+const { email, password } = event.currentTarget;
 
-if(inputEm.value === "" || inputPas.value === "") {
+if(email.value === "" || password.value === "") {
     return "Одно из полей пустое, пожалуйста заполните его!!!"
 }
-console.log(
-    `{Email: ${inputEm.value}, Password: ${inputPas.value}}`
-);
+
+const user = {
+    elements: {
+        email: email.value,
+        password: password.value
+    }
+}
+
+console.log(user.elements);
 event.currentTarget.reset();
 })
 
